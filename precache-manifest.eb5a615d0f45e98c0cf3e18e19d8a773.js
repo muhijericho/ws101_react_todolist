eb3dbbda0c15038ebf5a16021475915c4896f506
Your NameJericho <jerichomuhi@gmail.com>
2024-09-27 13:24:57 +0800

    Updates

---
+++ precache-manifest.eb5a615d0f45e98c0cf3e18e19d8a773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9244a69ec78dd4f81f1da7848b1096",
+    "url": "/ws101_react_todolist/index.html"
+  },
+  {
+    "revision": "698ecbb3fa59f7e7ee5e",
+    "url": "/ws101_react_todolist/static/css/main.37743b5b.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/ws101_react_todolist/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "698ecbb3fa59f7e7ee5e",
+    "url": "/ws101_react_todolist/static/js/main.427b668d.chunk.js"
+  },
+  {
+    "revision": "2f2f338abf3f575b3688",
+    "url": "/ws101_react_todolist/static/js/runtime~main.ce3e8571.js"
+  }
+]);
